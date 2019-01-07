d6b68dac179dd699a191697c78ce7f41c829571d
Stanley <ss95159@gmail.com>
2019-01-07 22:22:05 +0800

    調整css

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
+let box = document.querySelector('.box');
+let icon = document.querySelector('.icon');
+let header = document.querySelector('.header');
+icon.addEventListener('click',function(){
+  box.classList.toggle('show');
+  header.classList.toggle('m_header');
+  document.body.classList.toggle('body_fix');
 
-    let box = document.querySelector('.box');
-    let icon = document.querySelector('.icon');
-    let header = document.querySelector('.header');
-    icon.addEventListener('click',function(){
-      box.classList.toggle('show');
-      header.classList.toggle('m_header');
-      document.body.classList.toggle('body_fix');
-
-    });
-    let boxLi = document.querySelectorAll('.box > Li');
-    boxLi.forEach(function(item){
-      item.addEventListener('click',function (){
-        this.querySelector('.sub_box').classList.toggle('ul_show');
-      });
-    });
+});
+let boxLi = document.querySelectorAll('.box > Li');
+boxLi.forEach(function(item){
+  item.addEventListener('click',function (){
+    this.querySelector('.sub_box').classList.toggle('ul_show');
+  });
+});
